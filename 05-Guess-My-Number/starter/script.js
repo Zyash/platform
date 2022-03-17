47807394f43fd1bb23650e1977771f2c14b9fb51
Zyash <yashkumar.patel@ucalgary.ca>
2022-03-16 19:24:40 -0600

    Complete Game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,6 +3,7 @@
 let userNum;
 let scrtNum = Math.ceil(Math.random()*20);
 let score = 20;
+let highScore = 0;
 console.log('secret number is ' + scrtNum);
 
 let check = document.querySelector('.check').addEventListener('click',function(params) {
@@ -14,6 +15,15 @@ let check = document.querySelector('.check').addEventListener('click',function(p
     }
     else if(userNum == scrtNum){
         document.querySelector('.message').textContent = `you guessed correct answer ${userNum}`;
+        document.querySelector('body').style.backgroundColor = 'green';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').innerHTML = scrtNum;
+
+        if(score > highScore){
+            highScore = score;
+            document.querySelector('.label-highscore').innerHTML = `🥇 Highscore: ${highScore} `;
+
+        }
     }
 
     else if(userNum > scrtNum){
@@ -41,3 +51,16 @@ let check = document.querySelector('.check').addEventListener('click',function(p
         }
     }
 }) 
+
+let again = document.querySelector('.again').addEventListener('click',function(){
+    console.log('reset button has been clicked!');
+    score = 20;
+    document.querySelector('.label-score').innerHTML = '💯 Score: 20';
+    document.querySelector('.message').innerHTML = 'Start guessing...';
+    document.querySelector('.guess').value = '';
+    scrtNum = Math.ceil(Math.random()*20);
+    console.log(`secret number is ${scrtNum}`);
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').innerHTML = "?"
+});
