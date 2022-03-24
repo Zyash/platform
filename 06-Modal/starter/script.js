efeda92eaa80a2451a5bc0c4de51c59718bc72b7
Zyash <yashkumar.patel@ucalgary.ca>
2022-03-24 15:08:28 -0600

    Some chanes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+let close = document.querySelector('.close-modal');
+let show = document.querySelectorAll('.show-modal');
+console.log(show);
+
+for(let i=0;i<show.length;i++){
+        show[i].addEventListener('click', function () {
+        modal.classList.remove('hidden');
+        console.log('clicked button no.', show[i]);
+        overlay.classList.remove('hidden');
+    });
+
+        close.addEventListener('click', overLay)
+        overlay.addEventListener('click',overLay);
+}
+
+function overLay() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
